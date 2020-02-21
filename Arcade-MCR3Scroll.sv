b5c01729ba4df75657920a94fe5a9ac6f4c52dee
Miguel-T80c <the80scomputers@gmail.com>
2020-02-21 19:00:28 +0100

    DB9 OSD Support

--- Arcade-MCR3Scroll.sv
+++ Arcade-MCR3Scroll.sv
@@ -95,8 +95,9 @@ module emu
 	// 1 - D-/TX
 	// 2..6 - USR2..USR6
 	// Set USER_OUT to 1 to read from USER_IN.
-	output	      USER_MODE,
-	input   [7:0] USER_IN,
+output	USER_OSD,
+output	USER_MODE,
+input   [7:0] USER_IN,
 	output  [7:0] USER_OUT
 );
 
@@ -106,6 +107,7 @@ wire   joy_split, joy_mdsel;
 wire   [5:0] joy_in = {USER_IN[6],USER_IN[3],USER_IN[5],USER_IN[7],USER_IN[1],USER_IN[2]};
 assign USER_OUT  = |status[31:30] ? {3'b111,joy_split,3'b111,joy_mdsel} : '1;
 assign USER_MODE = |status[31:30] ;
+assign USER_OSD  = joydb9md_1[7] & joydb9md_1[5];
 
 assign LED_USER  = ioctl_download;
 
@@ -170,7 +172,7 @@ wire [10:0] ps2_key;
 
 wire [31:0] joy1_USB, joy2_USB;
 wire [31:0] joy1 = |status[31:30] ? {
-	joydb9md_1[8] | (joydb9md_1[7] & joydb9md_1[6]) , // Coin -> Mode | Start + A 	
+	joydb9md_1[8] | (joydb9md_1[7] & joydb9md_1[4]) , // Coin -> Mode | Start + B 	
 	joydb9md_1[11],// btn_shift	-> Z
 	joydb9md_1[7] | joydb9md_1[10], // btn_fireE -> Start | Y
 	joydb9md_1[9], // btn_fireD -> X
@@ -185,7 +187,7 @@ wire [31:0] joy1 = |status[31:30] ? {
 	: joy1_USB;
 
 wire [31:0] joy2 =  status[31]    ? {
-	joydb9md_2[8] | (joydb9md_1[7] & joydb9md_1[6]) , // Coin -> Mode | Start + A 	
+	joydb9md_2[8] | (joydb9md_1[7] & joydb9md_1[4]) , // Coin -> Mode | Start + B 	
 	joydb9md_2[11],// btn_shift	-> Z
 	joydb9md_2[7] | joydb9md_2[10], // btn_fireE -> Start | Y
 	joydb9md_2[9], // btn_fireD -> X
@@ -241,7 +243,8 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.joystick_1(joy2_USB),
 	.joystick_analog_0(joy1a),
 	.joystick_analog_1(joy2a),
-	.ps2_key(ps2_key)
+	.joy_raw(joydb9md_1[5:0]),
+.ps2_key(ps2_key)
 );
 
 wire [15:0] rom_addr;
